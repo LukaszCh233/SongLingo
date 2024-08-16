14b0ea6c8f4634ed0e0af03e354cea806276732b
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-08-16 20:19:16 +0200

    add project to gitHub

---
+++ src/main/java/com/example/SongLingo/SongLingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SongLingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongLingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongLingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SongLingo/SongLingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SongLingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongLingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
