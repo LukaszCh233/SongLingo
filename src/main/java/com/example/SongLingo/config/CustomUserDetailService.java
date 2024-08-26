3e309df0626dbfed170e129674afd8092da1d621
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-08-26 10:39:38 +0200

    add SecurityConfig

---
+++ src/main/java/com/example/SongLingo/config/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package com.example.SongLingo.config;
+
+import com.example.SongLingo.user.entity.User;
+import com.example.SongLingo.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        throw new UsernameNotFoundException("User not found with email: " + email);
+    }
+}
